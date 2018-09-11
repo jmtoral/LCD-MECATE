a606de622e561d23aaccaa32d065693d422745ed
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2018-09-11 18:45:58 -0500

    Created DB. (Falta contenido crudo y reparar el código... Pero ahí va. :rocket:)

--- pdf_extraction/extractor.py
+++ pdf_extraction/extractor.py
@@ -1,9 +1,10 @@
 from glob import glob
+import pandas as pd
 import pdftotext
 import re
 
 # Global:
-path_to_pdfs = "..."
+path_to_pdfs = "../y_2017"
 
 
 def list_pdfs(pdf_path):
@@ -29,45 +30,70 @@ def pdf_to_text(pdf_filename, to_text=False):
     return pdf_txt
 
 
-def extract_contracts(pdf_filename, to_text=False, cont_list=False):
+def extract_from_contracts(pdf_filename, to_text=False, cont_list=False):
     """Extract contract codes from given PDFs."""
 
-    # Set contract (regular expression) rule:
-    regex = r'[A-Za-z]+\/[A-Za-z]+\/[A-Za-z0-9]+\/[A-Za-z0-9]+\/[A-Za-z0-9]+'
-    extractor = re.compile(regex)
+    # Set contract rules:
+    cn_re = r'[A-Za-z]+\/[A-Za-z]+\/[A-Za-z0-9]+\/[A-Za-z0-9]+\/[A-Za-z0-9]+'
+    ta_re = r'Auditoría (.*?): (.*?)\n(.*?)\n'
+    contract_ext = re.compile(cn_re)
+    meta_ext = re.compile(ta_re)
 
     # Find all contracts in PDF file:
     pdf_txt = pdf_to_text(pdf_filename, to_text)
-    contracts_raw = extractor.findall(pdf_txt)
-    contracts = "\n".join(contracts_raw)
+    contracts_raw = contract_ext.findall(pdf_txt)
+    meta_raw = meta_ext.findall(pdf_txt)
+    contracts = list(set(contracts_raw))
+    try:
+        meta = [item for item in meta_raw[0]]
+    except:
+        meta = []
 
     # If specified, save output list:
     if cont_list:
         with open(pdf_filename[:-4] + '_contratos.txt', "w") as f:
             f.write(contracts)
 
-    return contracts
+    return meta, contracts, pdf_txt
 
 
-def extract_from_dir(dir_path, to_text=False, con_list=False):
+def extract_from_dir(dir_path, to_text=False, con_list=False, out="out.csv"):
     """Contracts extractor from given directory."""
 
+    # Create empty DataFrame:
+    # columnas = ['id_auditoria', 'numero', 'nombre_archivo', 'tipo_auditoria',
+    #             'contratos', 'contenido_crudo']
+    columnas = ['id_auditoria', 'numero', 'nombre_archivo', 'tipo_auditoria',
+                'contratos']
+    data = []
+
+    # Iterate over pdfs:
     pdfs_w_contracts = 0
     pdf_list = list_pdfs(dir_path)
     for i, pdf in enumerate(pdf_list):
         print("PDF {} from {}.".format(i, len(pdf_list)))
-        contracts = extract_contracts(pdf, to_text, con_list)
-        if len(contracts) > 0:
-            pdfs_w_contracts += 1
+        meta, contracts, pdf_txt = extract_from_contracts(
+            pdf, to_text, con_list)
+        pdf_txt = re.sub('"', '\"', pdf_txt)
+        try:
+            # data.append([meta[1], meta[2], pdf, meta[0], contracts, pdf_txt])
+            data.append([meta[1], meta[2], pdf, meta[0], contracts])
+            if len(contracts) > 0:
+                pdfs_w_contracts += 1
+        except:
+            # data.append([None, None, pdf, None, contracts, pdf_txt])
+            data.append([None, None, pdf, None, contracts])
 
+    df = pd.DataFrame(data, columns=columnas)
+    df.to_csv(out)
     if pdfs_w_contracts > 0:
         output_message = "Contracts found in {} PDFs!".format(pdfs_w_contracts)
     else:
         output_message = "No contracts found in this directory!"
-    return output_message
+    return df
 
 
 if __name__ == '__main__':
-    # pdf_list = list_pdfs("../y_2017")
-    # print(extract_contracts(pdf_list[0]))
-    print(extract_from_dir(path_to_pdfs, to_text=True, con_list=True))
+    # pdf_list = list_pdfs(path_to_pdfs)
+    # print(extract_from_contracts(pdf_list[0])[:2])
+    print(extract_from_dir("../y_2017"))
