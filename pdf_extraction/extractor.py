33f8e3a7a8b2beae8b00bc3a447c325688fdf440
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2018-09-13 21:29:37 -0500

    Updated database (ya quedó muy cool). :rocket:

--- pdf_extraction/extractor.py
+++ pdf_extraction/extractor.py
@@ -36,8 +36,10 @@ def extract_from_contracts(pdf_filename, to_text=False, cont_list=False):
     # Set contract rules:
     cn_re = r'[A-Za-z]+\/[A-Za-z]+\/[A-Za-z0-9]+\/[A-Za-z0-9]+\/[A-Za-z0-9]+'
     ta_re = r'Auditoría (.*?): (.*?)\n(.*?)\n'
+    ta_re_nl = r'Auditoría (.*?): (.*?)\n(.*?)\n(.*?)\n'
     contract_ext = re.compile(cn_re)
     meta_ext = re.compile(ta_re)
+    meta_ext2 = re.compile(ta_re_nl)
 
     # Find all contracts in PDF file:
     pdf_txt = pdf_to_text(pdf_filename, to_text)
@@ -46,6 +48,18 @@ def extract_from_contracts(pdf_filename, to_text=False, cont_list=False):
     contracts_raw = contract_ext.findall(pdf_txt)
     meta_raw = meta_ext.findall(pdf_txt)
     contracts = list(set(contracts_raw))
+
+    try:
+        if 'GB' in meta_raw[0][2] or 'DS' in meta_raw[0][2] or \
+           'DE' in meta_raw[0][2] or 'GF' in meta_raw[0][2]:
+            pass
+        else:
+            meta_raw = meta_ext2.findall(pdf_txt)
+            meta_raw = [(meta_raw[0][0], meta_raw[0][1] +
+                         meta_raw[0][2], meta_raw[0][3])]
+    except Exception as e:
+        pass
+
     try:
         meta = [item for item in meta_raw[0]]
     except Exception as e:
@@ -62,35 +76,35 @@ def extract_from_contracts(pdf_filename, to_text=False, cont_list=False):
 def extract_from_dir(dir_path, to_text=False, con_list=False, out="out.csv"):
     """Contracts extractor from given directory."""
 
-    # Create empty DataFrame:
+    # Create empty arrays:
     columnas = ['id_auditoria', 'numero', 'nombre_archivo', 'tipo_auditoria',
                 'contratos', 'contenido_crudo']
-    # columnas = ['id_auditoria', 'numero', 'nombre_archivo', 'tipo_auditoria',
-    #             'contratos']
     data = []
 
     # Iterate over pdfs:
     pdfs_w_contracts = 0
     pdf_list = list_pdfs(dir_path)
     for i, pdf in enumerate(pdf_list):
-        print("PDF {} from {}.".format(i, len(pdf_list)))
+        print("Processing PDF {} of {}.".format(i, len(pdf_list)))
         meta, contracts, pdf_txt = extract_from_contracts(
             pdf, to_text, con_list)
+        filename = pdf.split("/")[-1]
         try:
-            data.append([meta[1], meta[2], pdf, meta[0], contracts, pdf_txt])
-            # data.append([meta[1], meta[2], pdf, meta[0], contracts])
+            vector = [meta[1], meta[2], filename, meta[0], contracts, pdf_txt]
+            data.append(vector)
             if len(contracts) > 0:
                 pdfs_w_contracts += 1
         except Exception as e:
-            # data.append([None, None, pdf, None, contracts, pdf_txt])
-            data.append([None, None, pdf, None, contracts])
+            data.append([None, None, filename, None, contracts, pdf_txt])
 
     df = pd.DataFrame(data, columns=columnas)
     df.to_csv(out)
-    if pdfs_w_contracts > 0:
-        output_message = "Contracts found in {} PDFs!".format(pdfs_w_contracts)
-    else:
-        output_message = "No contracts found in this directory!"
+
+    # if pdfs_w_contracts > 0:
+    #     output_message = "{} contracts found!".format(pdfs_w_contracts)
+    # else:
+    #     output_message = "No contracts found in this directory!"
+
     return df
 
 
